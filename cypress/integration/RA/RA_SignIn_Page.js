c3515e98b9b9b34f5cbbcdf2f64843aa638be175
guettamaster <guettamaster@gmail.com>
2021-02-25 16:50:12 +0200

    add to index.js 'uncaught:exception' property

--- cypress/integration/RA/RA_SignIn_Page.js
+++ cypress/integration/RA/RA_SignIn_Page.js
@@ -4,11 +4,13 @@ describe("R&A Sign in", function() {
     it("Sign in", function() {
 
         cy.visit("https://test-dashboard.getwhistler.io/");;
-        cy.viewport(1366, 768);
+        cy.viewport(1920, 1080);
         cy.get(Locators.EMAIL_INPUT_FIELD).type("o.ivankovckyi@aimprosoft.com");
         cy.get(Locators.NEXT_BUTTON).click();
         cy.get(Locators.PASSWORD_INPUT_FIELD).type("Ivanoleg150288#");
         cy.contains("Sign In").click();
-        
+        cy.contains("Categories").should("be.visible");
+        cy.get(".navbar-header > a > i").click();
+        cy.contains("Oleg Ivankovskyi").should("be.visible");
     })
 })
